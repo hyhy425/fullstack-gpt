dcf852de9204682032b61294f7d073fd21d7127d
hyhy425 <hyhy425@naver.com>
2025-11-28 15:04:08 +0900

    6

---
+++ app1.py
@@ -0,0 +1,399 @@
+import json
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.callbacks import StreamingStdOutCallbackHandler
+import streamlit as st
+from langchain.retrievers import WikipediaRetriever
+
+
+function = {
+    "name": "create_quiz",
+    "description": "function that takes a list of questions and answers and returns a quiz",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "questions": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "question": {
+                            "type": "string",
+                        },
+                        "answers": {
+                            "type": "array",
+                            "items": {
+                                "type": "object",
+                                "properties": {
+                                    "answer": {
+                                        "type": "string",
+                                    },
+                                    "correct": {
+                                        "type": "boolean",
+                                    },
+                                },
+                                "required": ["answer", "correct"],
+                            },
+                        },
+                    },
+                    "required": ["question", "answers"],
+                },
+            }
+        },
+        "required": ["questions"],
+    },
+}
+
+
+st.set_page_config(
+    page_title="QuizGPT",
+    page_icon="❓",
+)
+
+st.title("QuizGPT")
+
+
+@st.cache_data(show_spinner="Validating API key...")
+def validate_api_key(api_key):
+    test_llm = ChatOpenAI(
+        model="gpt-4o-mini",
+        openai_api_key=api_key,
+        temperature=0.1,
+    )
+    test_llm.invoke("test")
+    return True
+
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+questions_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+    You are a helpful assistant that is role playing as a teacher.
+
+    The user wants questions of this difficulty level: {difficulty}
+    - easy: newbie
+    - hard: pro
+         
+    Based ONLY on the following context make 5 (FIVE) questions to test the user's knowledge about the text.
+    
+    Each question should have 4 answers, three of them must be incorrect and one should be correct.
+         
+    Use (o) to signal the correct answer.
+         
+    Question examples:
+         
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+         
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+         
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+         
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+         
+    Your turn!
+         
+    Context: {context}
+""",
+        )
+    ]
+)
+
+
+formatting_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+    You are a powerful formatting algorithm.
+     
+    You format exam questions into JSON format.
+    Answers with (o) are the correct ones.
+
+    Return the result as arguments for the `create_quiz` function.
+     
+    Example Input:
+
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+         
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+         
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+         
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+    
+     
+    Example Output:
+     
+    ```json
+    {{ "questions": [
+            {{
+                "question": "What is the color of the ocean?",
+                "answers": [
+                        {{
+                            "answer": "Red",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Yellow",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Green",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Blue",
+                            "correct": true
+                        }}
+                ]
+            }},
+                        {{
+                "question": "What is the capital or Georgia?",
+                "answers": [
+                        {{
+                            "answer": "Baku",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Tbilisi",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "Manila",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Beirut",
+                            "correct": false
+                        }}
+                ]
+            }},
+                        {{
+                "question": "When was Avatar released?",
+                "answers": [
+                        {{
+                            "answer": "2007",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "2001",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "2009",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "1998",
+                            "correct": false
+                        }}
+                ]
+            }},
+            {{
+                "question": "Who was Julius Caesar?",
+                "answers": [
+                        {{
+                            "answer": "A Roman Emperor",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "Painter",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Actor",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Model",
+                            "correct": false
+                        }}
+                ]
+            }}
+        ]
+     }}
+    ```
+    Respond ONLY with the JSON object, no explanation.
+
+    Your turn!
+
+    Questions: {context}
+
+""",
+        )
+    ]
+)
+
+
+@st.cache_data(show_spinner="Loading file...")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/quiz_files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
+
+
+@st.cache_data(show_spinner="Making quiz...")
+def run_quiz_chain(_docs, topic, difficulty):
+    chain = {"context": questions_chain} | formatting_chain
+    response = chain.invoke({"docs": _docs, "difficulty": difficulty})
+    response = response.additional_kwargs["function_call"]["arguments"]
+    quiz = json.loads(response)
+    return quiz
+
+
+@st.cache_data(show_spinner="Searching Wikipedia...")
+def wiki_search(term):
+    retriever = WikipediaRetriever(top_k_results=5)
+    docs = retriever.get_relevant_documents(term)
+    return docs
+
+
+with st.sidebar:
+    docs = None
+    topic = None
+
+    st.markdown("📦 [GitHub Repository](https://github.com/hyhy425/fullstack-gpt)")
+
+    api_key = st.text_input(
+        "Enter your OpenAI API Key",
+        type="password",
+        placeholder="sk-...",
+    )
+
+    difficulty = st.selectbox(
+        "Choose difficulty",
+        (
+            "easy",
+            "hard",
+        ),
+    )
+    choice = st.selectbox(
+        "Choose what you want to use.",
+        (
+            "File",
+            "Wikipedia Article",
+        ),
+    )
+    if choice == "File":
+        file = st.file_uploader(
+            "Upload a .docx , .txt or .pdf file",
+            type=["pdf", "txt", "docx"],
+        )
+        if file:
+            docs = split_file(file)
+    else:
+        topic = st.text_input("Search Wikipedia...")
+        if topic:
+            docs = wiki_search(topic)
+
+
+if not api_key:
+    st.warning("Please enter your API key in the sidebar.")
+    st.stop()
+
+try:
+    validate_api_key(api_key)
+except Exception:
+    st.error("Invalid API Key.")
+    st.stop()
+
+
+llm = ChatOpenAI(
+    model="gpt-4o-mini",
+    openai_api_key=api_key,
+    temperature=0.1,
+    streaming=True,
+    callbacks=[StreamingStdOutCallbackHandler()],
+)
+
+llm_quiz = ChatOpenAI(
+    model="gpt-4o-mini",
+    openai_api_key=api_key,
+    temperature=0.1,
+).bind(
+    function_call={
+        "name": "create_quiz",
+    },
+    functions=[
+        function,
+    ],
+)
+
+
+questions_chain = (
+    {
+        "context": lambda x: format_docs(x["docs"]),
+        "difficulty": lambda x: x["difficulty"],
+    }
+    | questions_prompt
+    | llm
+)
+
+formatting_chain = formatting_prompt | llm_quiz
+
+
+if not docs:
+    st.markdown(
+        """
+    Welcome to QuizGPT.
+                
+    I will make a quiz from Wikipedia articles or files you upload to test your knowledge and help you study.
+                
+    Get started by uploading a file or searching on Wikipedia in the sidebar.
+    """
+    )
+else:
+    response = run_quiz_chain(docs, topic if topic else file.name, difficulty)
+
+    total = len(response["questions"])
+    score = 0
+
+    with st.form("questions_form"):
+        score = 0
+
+        for question in response["questions"]:
+            st.write(question["question"])
+            value = st.radio(
+                "Select an option.",
+                [answer["answer"] for answer in question["answers"]],
+                index=None,
+            )
+            if {"answer": value, "correct": True} in question["answers"]:
+                st.success("Correct!")
+                score += 1
+            elif value is not None:
+                st.error("Wrong!")
+        button = st.form_submit_button()
+
+    if button:
+        st.write(f"{score} / {total}")
+
+        if score == total:
+            st.success("good")
+            st.balloons()
+        else:
+            st.warning("retry")
